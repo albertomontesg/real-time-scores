148e52a78baa79203d66b6fdaab8d6325fea179e
Alberto Montes <alm59321@gmail.com>
2015-12-20 13:26:50 +0100

    Add Team's model

--- information/models.py
+++ information/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db.models import Model, CharField
+from django.conf import settings
 
-# Create your models here.
+class Team(Model):
+    GENDERS = (
+        ('M', u'Masculí'),
+        ('F', u'Femení')
+    )
+
+    name = CharField(max_length=100, blank=False)
+    gender = CharField(max_length=10, choices=GENDERS, default='M')
+    url_id = CharField(max_length=100, blank=True)
+
+    def get_url(self, day):
+        ' The day is "jornada"'
+        assert isinstance(day, int) or isinstance(day, str), 'Day must be and integer or a string'
+        return settings.FCB_COMPETITIONS_URL + self.url_id + '/' + str(day)
+
+    def full_name(self):
+        return self.name + ' ' + self.gender
